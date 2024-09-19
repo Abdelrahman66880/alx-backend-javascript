2fde545802dd371618d4ada6e3ce1dccef95eb61
Abdelrahman <abdofathi317@gmail.com>
2024-09-19 04:24:31 -0700

    Fix the code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,17 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-});
+if (process.stdin.isTTY) {
+        process.stdin.on('data', (data) => {
+                process.stdout.write(`Your name is: ${data.toString()}`);
+                process.exit();
+        });
+} else {
+        process.stdin.on('data', (data) => {
+                process.stdout.write(`Your name is: ${data.toString()}`);
+                process.exit();
+        })
+};
+process.on('exit', () => {
+        process.stdout.write(`This important software is now closing\n`);
+        process.exit();
+})
